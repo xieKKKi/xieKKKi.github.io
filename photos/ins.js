3edfc9f67d6acd06c53fdfe3539962738202d13b
xiekkk <xiekkk@yeah.net>
2021-04-29 17:17:19 +0800

    Site updated: 2021-04-29 17:17:18

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/xieKKKi/myBlogImages/tree/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/xieKKKi/myBlogImages/tree/master/photos/' + data.link[i];
+          var minSrc = 'https://github.com/xieKKKi/myBlogImages/blob/master/min_photos/' + data.link[i];
+          var src = 'https://github.com/xieKKKi/myBlogImages/blob/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
